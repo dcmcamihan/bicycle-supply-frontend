45163a3ff9835e8ad46c7c68ecf3524d501c1f21
Sherwin Amihan <sherwin.amihan@aboitizpower.com>
2025-10-30 21:47:00 +0800

    Installed serve dependency.

--- vite.config.mjs
+++ vite.config.mjs
@@ -16,6 +16,6 @@ export default defineConfig({
   port: "4028",
   host: "0.0.0.0",
   strictPort: true,
-  allowedHosts: ["all"]
+  allowedHosts: ["all", "bicycle-supply-frontend.onrender.com"]
   }
 });
