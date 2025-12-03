eea0d4ad9f85e10740e328b6e5c2313653bfd327
Dianna Claire Marie Amihan <diannaclairemarie.amihan@gmail.com>
2025-12-03 15:23:15 +0800

    Fixed inventory management structure.

--- src/pages/inventory-management/index.jsx
+++ src/pages/inventory-management/index.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
-import { useLocation } from 'react-router-dom';
+import { useRoutes } from 'react-router-dom';
 import InventoryList from './pages/InventoryList';
 import ProductDetails from '../product-details';
 
 const InventoryManagement = () => {
-  const location = useLocation();
-  
-  // Check if we're on product-details subpage
-  const isProductDetailsPage = location.pathname.includes('/product-details');
+  const routes = useRoutes([
+    { index: true, element: <InventoryList /> },
+    { path: 'product-details', element: <ProductDetails /> }
+  ]);
 
-  return isProductDetailsPage ? <ProductDetails /> : <InventoryList />;
+  return routes;
 };
 
 export default InventoryManagement;
